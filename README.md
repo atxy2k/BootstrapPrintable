83ce363d9fe31b0bfc621c010fdec2e7e7ec06f2
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2017-12-06 14:50:18 -0600

    end the integration

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This library intent create a presentation of html data before the user download a PDF object, 
 born of needing the don't process pdf in the server, we had this idea.
+
+Only tested on chrome web browser.
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 require('./src/scss/app.scss');
+import $ from 'jQuery';
+
+$('#print-button').on('click', function(evt){
+    if( 'preventDefault' in evt ) evt.preventDefault();
+    if( 'stopPropagation' in evt ) evt.stopPropagation();
+    window.print();
+});
