cb330d657b73e9b940e12bf7d8688809a87e0e44
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2017-12-07 17:52:05 -0600

    Terminando funcionalidad

--- src/scripts/Component.js
+++ src/scripts/Component.js
@@ -1,6 +1,6 @@
 import $ from 'jQuery';
-import { throw_if, throw_unless, in_array } from './Utils';
-import {isEmpty,forEach, range} from 'lodash';
+import { throw_if, throw_unless, in_array, number_month, month_from_number } from './Utils';
+import {isEmpty,forEach, range, isNull, find,has,isObject} from 'lodash';
 import moment from 'moment';
 moment.locale('es-ES');
 
@@ -25,7 +25,14 @@ let DefaultOptions =
     },
     date : {
         days : range(1,31),
-        years : range( moment().year() - 5, moment().year() + 1 ).reverse()
+        years : range( moment().year() - 10, moment().year() + 1 ).reverse(),
+        format : 'MMMM DD, YYYY.'
+    }, 
+    options : 
+    {
+        onChange : null, //When event change
+        preBuild : null, //Clean value
+        commit    : null
     }
 };
 class Component
@@ -35,6 +42,11 @@ class Component
     {
         this.$component = $component;
         this.options = $.extend({}, DefaultOptions, options);
+        this.provisional_date = {
+            day     : null,
+            month   : null,
+            year    : null
+        };
         let types_availables = ['text','textarea','select','date','select-ajax','select-object'];
         let data_target_name = $component.attr('data-target-name'); //Name
         let data_target_type = $component.attr('data-target-type'); //Type
@@ -71,6 +83,17 @@ class Component
         {
             data_target_default = !isEmpty(data_target_default) ? JSON.parse(data_target_default) : null;
         }
+        else if( data_target_type === 'date' && !isEmpty(data_target_default) )
+        {
+            let provisional = data_target_default.split('-');
+            console.log(month_from_number(parseInt(provisional[1])));
+            data_target_default = 
+            { 
+                year : provisional[0],
+                month : month_from_number(parseInt(provisional[1])),
+                day : provisional[2],
+            };
+        }
         data_target_required = data_target_required === 'true';
         this.properties = 
         { 
@@ -100,8 +123,8 @@ class Component
                         self.properties.origin = data;
                         let html = template(self.properties);
                         let element = html.toDOM();
-                        self.$element = $(element);
-                        $form.find('.Modal-settings-content').append( self.$element );
+                        $form.find('.Modal-settings-content').append( $(element) );
+                        self.$element = $form.find('.Modal-settings-content').children().last();
                         resolve( self.$element );    
                     }).catch(function(err){
                         reject(err);
@@ -111,8 +134,8 @@ class Component
                 {
                     let html = template(self.properties);
                     let element = html.toDOM();
-                    self.$element = $(element);
-                    $form.find('.Modal-settings-content').append( self.$element );
+                    $form.find('.Modal-settings-content').append( $(element) );
+                    self.$element = $form.find('.Modal-settings-content').children().last();
                     resolve( self.$element );
                 }
             }
@@ -121,6 +144,151 @@ class Component
                 reject(err);
             }
         });
+    }
+
+    bindEvents($manager)
+    {
+        let self = this;
+        if( this.properties.type == 'text' )
+        {
+            this.$element.find('input[type=text]').on('keyup', function( evt ){
+                if( !isNull(self.options.options.onChange) ) self.options.options.onChange( self.properties.name, evt, $(this).val(), self.properties, self.$element );
+            });
+        }
+        else if( this.properties.type == 'textarea' )
+        {
+            this.$element.find('textarea').on('keyup', function( evt ){
+                if( !isNull(self.options.options.onChange) ) self.options.options.onChange( self.properties.name, evt, $(this).val(), self.properties, self.$element );
+            });
+        }
+        else if( this.properties.type == 'select'  )
+        {
+            this.$element.find('select').on('change', function( evt ){
+                let value = $(this).val();
+                let result = find(self.properties.origin, function(item){
+                    return item.key === value;
+                });
+                if( !isNull(self.options.options.onChange) ) self.options.options.onChange( self.properties.name, evt, result, self.properties, self.$element );
+            });
+        }
+        else if( this.properties.type == 'select-object' || this.properties.type == 'select-ajax' )
+        {
+            this.$element.find('select').on('change', function( evt ){
+                let value = $(this).val();
+                let result = find(self.properties.origin, function(item){
+                    return item[self.properties.origin_key] == value;
+                });
+                if( !isNull(self.options.options.onChange) ) self.options.options.onChange( self.properties.name, evt, result, self.properties, self.$element );
+            });
+        }
+        else if( this.properties.type == 'date' )
+        {
+            this.$element.find('select').on('change', function(evt)
+            {
+                let value = $(this).val();
+                let type  = $(this).data('type');
+                if( type == 'day' )
+                {
+                    self.provisional_date.day = value <10 ? `0${value}` : ''+value;
+                }
+                else if( type == 'month' )
+                {
+                    let n = number_month(value);
+                    self.provisional_date.month = n <10 ? `0${n}` : ''+value;
+                }
+                else if( type == 'year' )
+                {
+                    self.provisional_date.year = value <10 ? `0${value}` : ''+value;
+                }
+                if( (!isNull( self.provisional_date.day ) && !isNull( self.provisional_date.month ) && !isNull( self.provisional_date.year )) && (!isEmpty( self.provisional_date.day ) && !isEmpty( self.provisional_date.month ) && !isEmpty( self.provisional_date.year )) )
+                {
+                    let dateString = `${self.provisional_date.year}-${self.provisional_date.month}-${self.provisional_date.day}`;
+                    let result = moment(dateString);
+                    if( result.isValid() )
+                    {
+                        if( !isNull(self.options.options.onChange) ) self.options.options.onChange( self.properties.name, evt, result, self.properties, self.$element );
+                    }
+                }
+            });
+        }
+    }
+
+    commit()
+    {
+        let self = this;
+        let value = has(this.options.options.preBuild, this.properties.name )   ? 
+            this.options.options.preBuild[this.properties.name](this.getValue()) : this.getValue();
+        if( has(this.options.options.commit,this.properties.name) )
+        {
+            this.options.options.commit[this.properties.name]( 
+                value, 
+                this.$component,
+                this.properties,
+                this.$element
+            );
+        }
+        else
+        {
+            if( moment.isMoment(value) )
+            {
+                this.$component.html(value.format( this.options.date.format ));
+            }
+            else if( isObject( value ) )
+            {
+                if( this.properties.type == 'select' )
+                {
+                    this.$component.html(value.value);
+                }
+                else if( this.properties.type == 'select-object' || this.properties.type == 'select-ajax' )
+                {
+                    this.$component.html(value[this.properties.origin_value]);
+                }
+            }
+            else
+            {
+                this.$component.html( value );
+            }
+        }
+    }
+
+    getValue()
+    {
+        let self = this;
+        let response = null;
+        if( this.properties.type == 'text' )
+        {
+            return this.$element.find('input[type=text]').val();
+        }
+        else if( this.properties.type == 'textarea' )
+        {
+            return this.$element.find('textarea').val();
+        }
+        else if( this.properties.type == 'select' )
+        {
+            let value = this.$element.find('select').val();
+            return find(self.properties.origin, function(item){
+                return item.key === value;
+            });
+        }
+        else if( this.properties.type == 'select-object' || this.properties.type == 'select-ajax' )
+        {
+            let value = this.$element.find('select').val();
+            return find(self.properties.origin, function(item){
+                return item[self.properties.origin_key] == value;
+            });
+        }
+        else if( this.properties.type == 'date' )
+        {
+            if( (!isNull( self.provisional_date.day ) && !isNull( self.provisional_date.month ) && !isNull( self.provisional_date.year )) && (!isEmpty( self.provisional_date.day ) && !isEmpty( self.provisional_date.month ) && !isEmpty( self.provisional_date.year )) )
+            {
+                let dateString = `${self.provisional_date.year}-${self.provisional_date.month}-${self.provisional_date.day}`;
+                let result = moment(dateString);
+                if( result.isValid() )
+                {
+                    return result;
+                }
+            }
+        }
     }
 
 }
--- src/scripts/FormManager.js
+++ src/scripts/FormManager.js
@@ -1,6 +1,6 @@
 import $ from 'jQuery';
 import Component from './Component';
-import {isNull} from 'lodash';
+import {isNull, forEach} from 'lodash';
 class FormManager
 {
 
@@ -27,9 +27,18 @@ class FormManager
         $(this.selectors.handler).each(function(){
             let component = new Component( $(this), settings );
             component.build( $(self.selectors.modal).find('form') ).then(()=>{
+                component.bindEvents(self);
                 components.push(component);
             }, (err) => {
                 throw err;
+            });
+        });
+        $(this.selectors.modal).find('form').on('submit', function(evt){
+            if( 'preventDefault' in evt ) evt.preventDefault();
+            if( 'stopPropagation' in evt ) evt.stopPropagation();
+            forEach( components, function( component ){
+                component.commit();
+                $(self.selectors.modal).removeClass('visible');
             });
         });
     }
